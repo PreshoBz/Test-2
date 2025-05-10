dbb16ff62044415006ddc82f5b1d9b0a42728b9f
PreshoBz <2009118831@ub.edu.bz>
2025-05-10 17:37:53 -0600

    Added class and constructor

--- test2.cpp
+++ test2.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// Size of the hash table
+const int TABLE_SIZE = 10;
+
+//Hashtable class
+class HashTable {
+private:
+    int table[TABLE_SIZE];
+
+public:
+    // Constructor: initialize all slots to -1 (empty)
+    HashTable() {
+        for (int i = 0; i < TABLE_SIZE; ++i)
+            table[i] = -1;
+    }
 
